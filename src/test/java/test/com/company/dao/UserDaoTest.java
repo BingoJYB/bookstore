d54458bafee0415af4d51e8bfbc235918469cdbf
JYB <7303639+BingoJYB@users.noreply.github.com>
2022-05-26 16:00:39 +0200

    Add user service test cases

--- src/test/java/test/com/company/dao/UserDaoTest.java
+++ src/test/java/test/com/company/dao/UserDaoTest.java
@@ -4,31 +4,45 @@ import com.company.dao.impl.UserDao;
 import com.company.entity.User;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class UserDaoTest {
+
+    private UserDao userDao;
+
+    @BeforeEach
+    void setUp() {
+
+        userDao = new UserDao();
+    }
+
+    @Test
+    public void testDeleteUser() {
+
+        User user = new User(null, "admin", "123", "admin@gmail.com");
+
+        assertNotEquals(-1, userDao.deleteUser(user));
+    }
 
     @Test
     public void testSaveUser() {
 
         User user = new User(null, "admin", "123", "admin@gmail.com");
-        UserDao userDao = new UserDao();
 
-        System.out.println(userDao.saveUser(user));
+        assertNotEquals(-1, userDao.saveUser(user));
     }
 
     @Test
     public void testGetUserByUsername() {
 
-        UserDao userDao = new UserDao();
-
-        System.out.println(userDao.getUserByUsername("admin"));
+        assertNotNull(userDao.getUserByUsername("admin"));
     }
 
     @Test
     public void testGetUserByUsernameAndPassword() {
 
-        UserDao userDao = new UserDao();
-
-        System.out.println(userDao.getUserByUsernameAndPassword("admin", "123"));
+        assertNotNull(userDao.getUserByUsernameAndPassword("admin", "123"));
     }
 }
--- src/test/java/test/com/company/service/UserServiceTest.java
+++ src/test/java/test/com/company/service/UserServiceTest.java
@@ -1,5 +1,61 @@
 package test.com.company.service;
 
+import com.company.dao.impl.UserDao;
+import com.company.entity.User;
+import com.company.service.impl.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class UserServiceTest {
-    
+
+    private UserService userService;
+    private UserDao userDao;
+
+    @BeforeEach
+    void setUp() {
+
+        userService = new UserService();
+        userDao = new UserDao();
+    }
+
+    @Test
+    public void testDeregisterUser() {
+
+        User user = new User(null, "Iron Man", "321", "man.iron@gmail.com");
+
+        userService.deregisterUser(user);
+
+        assertNull(userDao.getUserByUsername(user.getUsername()));
+    }
+
+    @Test
+    public void testRegisterUser() {
+
+        User user = new User(null, "Iron Man", "321", "man.iron@gmail.com");
+
+        userService.registerUser(user);
+
+        assertNotNull(userDao.getUserByUsername(user.getUsername()));
+    }
+
+    @Test
+    public void testLogin() {
+
+        User user = new User(null, "Iron Man", "321", "man.iron@gmail.com");
+
+        assertNotNull(userService.login(user));
+    }
+
+    @Test
+    public void testIsUsernameDuplicated() {
+
+        assertTrue(userService.isUsernameDuplicated("Iron Man"));
+        assertFalse(userService.isUsernameDuplicated("Batman"));
+    }
+
 }
