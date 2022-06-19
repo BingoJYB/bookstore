e6c94b05cd5c9544d7e9bcab3f8c9514536cb6c6
JYB <7303639+BingoJYB@users.noreply.github.com>
2022-06-20 01:19:22 +0200

    Add order and order item sql

--- sql/bookstore.sql
+++ sql/bookstore.sql
@@ -23,3 +23,22 @@ CREATE TABLE t_book (
     stock int DEFAULT 0,
     img_path varchar(100)
 );
+
+CREATE TABLE t_order (
+    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    create_time timestamp DEFAULT CURRENT_TIMESTAMP,
+    price decimal(7, 2) NOT NULL,
+    status int DEFAULT 0,
+    user_id int NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES t_user(id)
+);
+
+CREATE TABLE t_order_item (
+    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name varchar(30) NOT NULL,
+    count int DEFAULT 0,
+    price decimal(7, 2) NOT NULL,
+    total_price decimal(7, 2) NOT NULL,
+    order_id int NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES t_order(id)
+);
