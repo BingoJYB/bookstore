24b890ac4069fb99ae04dd06723ac11edd0dc7e6
JYB <7303639+BingoJYB@users.noreply.github.com>
2022-06-21 01:27:27 +0200

    Implement order dao

---
+++ src/main/java/com/company/dao/impl/OrderDao.java
@@ -0,0 +1,24 @@
+package com.company.dao.impl;
+
+import com.company.dao.IOrderDao;
+import com.company.entity.Order;
+import com.company.entity.OrderItem;
+
+public class OrderDao extends BaseDao implements IOrderDao {
+
+    public int saveOrder(Order order) {
+
+        String query = "INSERT INTO t_order (create_time, price, status, user_id) VALUES (?, ?, ?, ?)";
+
+        return update(query, order.getCreateTime(), order.getPrice(), order.getStatus(), order.getUserId(),
+                order.getUserId());
+    }
+
+    public int saveOrderItem(OrderItem orderItem) {
+
+        String query = "INSERT INTO t_order_item (name, count, price, total_price, order_id) VALUES (?, ?, ?, ?, ?)";
+
+        return update(query, orderItem.getName(), orderItem.getCount(), orderItem.getPrice(), orderItem.getTotalPrice(),
+                orderItem.getOrderId());
+    }
+}
